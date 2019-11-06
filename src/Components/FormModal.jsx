219aaf4f19e1cd1f7469a4c5d752243232c625b3
James Angeles <JamesAngeles21@gmail.com>
2019-11-06 15:43:01 -0500

    created custom form modal and added modal support for creating courses and lessons

---
+++ src/Components/FormModal.jsx
@@ -0,0 +1,27 @@
+import React from 'react';  
+import '../Styles/popupStyle.css';  
+import { Button, Form, Modal } from 'react-bootstrap';
+
+class FormModal extends React.Component {  
+  render() {  
+    return (  
+        <Modal
+            {...this.props} 
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {this.props.title}
+                </Modal.Title>
+            </Modal.Header>
+            <Form>
+                {this.props.children}
+            </Form>
+        </Modal>
+
+    );  
+}  
+}  
+export default FormModal;
--- src/Pages/InstructorDashboard.jsx
+++ src/Pages/InstructorDashboard.jsx
@@ -5,12 +5,23 @@ import '../Styles/InstructorDashboard.css';
 import { Link } from 'react-router-dom';
 import HeaderBru from '../Components/Header.jsx';
 import Routes from '../utils/RouteConstants.js';
+import FormModal from '../Components/FormModal.jsx';
+import { Button, Form, Modal } from 'react-bootstrap';
+
 
 const links = {
 	Account: "/account",
 }
 
 export class InstructorDashboard extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			showCourseModal: false,
+			showLessonModal: false,
+		};
+	}
 
 	handleNext(){}
 	render() {
@@ -29,9 +40,16 @@ export class InstructorDashboard extends Component {
 						<div className="recentDrinksDiv">
 							<div className="recentDrinkTop">
 								<h4>Recent Lessons</h4>
-								<Link to="/Editor" className="link">
-								<button className="buttonRound btn-primary">+</button>
-								</Link>
+								<button className="buttonRound" onClick={() => {
+									this.setState({showLessonModal: true});
+								}}>
+									+
+								</button>
+								<FormModal title="Create Course" show={this.state.showLessonModal} onHide={() => {
+									this.setState({showLessonModal: false})
+								}}>
+									{this.getLessonForm()}
+								</FormModal>
 							</div>
 							<div className="recentDrinkBottom">
 								<ol>
@@ -72,9 +90,16 @@ export class InstructorDashboard extends Component {
 						<div className="recentLessonsDiv">
 							<div className="recentLessonsTop">
 								<h4>Recent Courses</h4>
-								<Link to="/course" className="link">
-								<button className="buttonRound btn-primary">+</button>
-								</Link>
+								<button className="buttonRound" onClick={() => {
+									this.setState({showCourseModal: true});
+								}}>
+									+
+								</button>
+								<FormModal title="Create Course" show={this.state.showCourseModal} onHide={() => {
+									this.setState({showCourseModal: false})
+								}}> 
+									{this.getCourseForm()}
+					            </FormModal>
 							</div>
 							<div className="recentLessonsBottom">
 								<div className="training1">
@@ -96,6 +121,42 @@ export class InstructorDashboard extends Component {
 					</div>
 				</div>
 			</div>
+		);
+	}
+
+	getCourseForm() {
+		return (
+			<div>
+				<Modal.Body>
+		            <Form.Group controlId="formBasicEmail">
+		                <Form.Label>Course Name</Form.Label>
+		                <Form.Control minlength="5" required placeholder="Enter Course Name"/>
+		            </Form.Group>
+		        </Modal.Body>
+		        <Modal.Footer>
+		            <Button variant="primary" type="submit">
+		                Create
+		            </Button>
+		        </Modal.Footer>
+	        </div>
+		);
+	}
+
+	getLessonForm() {
+		return (
+			<div>
+				<Modal.Body>
+		            <Form.Group controlId="formBasicEmail">
+		                <Form.Label>Lesson Name</Form.Label>
+		                <Form.Control minlength="5" required placeholder="Enter Lesson Name"/>
+		            </Form.Group>
+		        </Modal.Body>
+		        <Modal.Footer>
+		            <Button variant="primary" type="submit">
+		                Create
+		            </Button>
+		        </Modal.Footer>
+	        </div>
 		);
 	}
 }
