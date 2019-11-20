a979ea07c837ce15576a949e400e2b430ed8d740
James Angeles <JamesAngeles21@gmail.com>
2019-11-19 20:19:15 -0500

    created initial tool table

---
+++ backend/Bru/src/main/java/BruGroup/Bru/Entity/ToolIdentity.java
@@ -0,0 +1,40 @@
+package BruGroup.Bru.Entity;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+
+@Embeddable
+public class ToolIdentity implements Serializable {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private int stepID;
+
+    public ToolIdentity() {
+
+    }
+
+    public ToolIdentity(String toolID, int stepID) {
+        this.name = name;
+        this.stepID = stepID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStepID() {
+        return stepID;
+    }
+
+    public void setStepID(int stepID) {
+        this.stepID = stepID;
+    }
+}
