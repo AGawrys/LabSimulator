33b54771fe43053f7072ee6757673cb825ea9566
Steven Kuang <stevenkuang951@gmail.com>
2019-12-08 19:44:16 -0500

    loaded tool into tool editor

--- frontend/src/Components/Tool.jsx
+++ frontend/src/Components/Tool.jsx
@@ -1,89 +1,84 @@
-import React from "react";
+import React from 'react';
 import Draggable from 'react-draggable';
 import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 import EditorConstants from '../utils/EditorConstants.js';
 
 class Tool extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: false
-        }   
-        this.selected = props.selected;
+	constructor(props) {
+		super(props);
+		this.state = {
+			selected: false
+		};
+		this.selected = props.selected;
 
-        this.onClick = this.onClick.bind(this);
-        this.onDragStop = this.onDragStop.bind(this);
-        this.canvas = React.createRef();
-    }
+		this.onClick = this.onClick.bind(this);
+		this.onDragStop = this.onDragStop.bind(this);
+		this.canvas = React.createRef();
+	}
 
-    onClick(e) {
-        this.props.setCurrentTool(this.props.tool);
-        e.stopPropagation();
-    }
+	onClick(e) {
+		this.props.setCurrentTool(this.props.tool);
+		e.stopPropagation();
+	}
 
-    onDragStop(e, data) {
-        const {tool} = this.props;
-        const {x,y} = data;
-        tool.setPosition(x,y);
-    }
+	onDragStop(e, data) {
+		const { tool } = this.props;
+		const { x, y } = data;
+		tool.setPosition(x, y);
+	}
 
-    componentDidMount() {
-        const {tool} = this.props;
-        const image = tool.getImage();
-        image.draw(this.canvas.current,
-                   tool.getWidth(),
-                   tool.getHeight(),
-                   image.properties)
-    }
+	componentDidMount() {
+		const { tool } = this.props;
+		const image = tool.getImage();
+		image.draw(this.canvas.current, tool.getWidth(), tool.getHeight(), image.properties);
+	}
 
-    componentDidUpdate() {
-        this.componentDidMount()
-    }
+	componentDidUpdate() {
+		this.componentDidMount();
+	}
 
-    render() {
+	render() {
+		const { tool } = this.props;
+		let style = this.selected ? { border: '1px #7fb3d8 solid' } : { border: '1px transparent solid' };
+		const ToolCanvas = (
+			<canvas
+				width={tool.getWidth()}
+				height={tool.getHeight()}
+				style={style}
+				onClick={this.props.draggable ? this.onClick : null}
+				ref={this.canvas}
+			>
+				{tool.getName()}
+			</canvas>
+		);
 
-        const {tool} = this.props;
-        let style = this.selected ?
-            {border: "1px #7fb3d8 solid"} : {border: "1px transparent solid"};
-        const ToolCanvas = (
-            <canvas
-                width={tool.getWidth()}
-                height={tool.getHeight()}
-                style={style}
-                onClick={this.props.draggable? this.onClick : null}
-                ref={this.canvas}
-            >
-                {tool.getName()}
-            </canvas>
-        )
+		let ToolComponent = ToolCanvas;
+		if (this.props.draggable) {
+			ToolComponent = (
+				<Draggable
+					bounds={'#canvas'}
+					position={{ x: tool.getPosition().getX(), y: tool.getPosition().getY() }}
+					onStop={this.onDragStop}
+				>
+					{ToolCanvas}
+				</Draggable>
+			);
+		}
 
-        let ToolComponent = ToolCanvas;
-        if (this.props.draggable) {
-            ToolComponent = (
-                <Draggable
-                    bounds={"#canvas"}
-                    position={{x: tool.getPosition().getX(), y: tool.getPosition().getY()}}
-                    onStop={this.onDragStop}
-                >
-                    {ToolCanvas}
-                </Draggable>
-            )
-        }
-
-        return(
-            <div
-                style={{
-                    width: tool.getWidth(),
-                    height: tool.getHeight(),
-                    margin: 0,
-                    position: this.props.draggable? "absolute" : "relative",
-                }}
-            >
-                {ToolComponent}
-            </div>
-        );
-    }
+		return (
+			<div
+				style={{
+					width: tool.getWidth(),
+					height: tool.getHeight(),
+					margin: 0,
+					position: this.props.draggable ? 'absolute' : 'relative'
+				}}
+			>
+				{ToolComponent}
+			</div>
+		);
+	}
 }
 
 export default Tool;
-export {Tool}
+export { Tool };
--- frontend/src/Components/Tools.jsx
+++ frontend/src/Components/Tools.jsx
@@ -1,57 +1,156 @@
-import {LineCalculator} from "../utils/LilacAlgebra.js"
+import { LineCalculator } from '../utils/LilacAlgebra.js';
+import '../images/icecube.png';
 
 const CATEGORIES = {
-    Cups: [
-        "TaperedCup"
-    ]
+	Cups: [ 'TaperedCup', 'IceCube', 'Banana', 'Milk', 'Mango', 'Blueberry', 'Strawberry', 'Machine' ]
 };
 
 const IMAGES = {
-    TaperedCup: {
-        draw: (canvas, width, height, properties) => {
-            if (canvas.getContext) {
-                let context = canvas.getContext("2d");
+	TaperedCup: {
+		draw: (canvas, width, height, properties) => {
+			if (canvas.getContext) {
+				let context = canvas.getContext('2d');
 
-                const bounds = canvas.getBoundingClientRect();
-                context.clearRect(0, 0, bounds.width, bounds.height);
-    
-                context.beginPath();
-                context.moveTo(width * .1, height * .05);
-                context.lineTo(width * .2, height * .95);
-                context.lineTo(width * .8, height * .95);
-                context.lineTo(width * .9, height * .05);
-                context.stroke()
-    
-                const left = LineCalculator(width * .1, height * .05, width * .2, height * .95)
-                const right = LineCalculator(width * .8, height * .95, width * .9, height * .05)
-                
-                const cupHeight = (height * .95) - (height * .05)
-                const fillPoint = (height * .05) + (cupHeight * (1 - properties.Fill))
+				const bounds = canvas.getBoundingClientRect();
+				context.clearRect(0, 0, bounds.width, bounds.height);
 
-                context.beginPath();
-                context.moveTo(left.x(fillPoint), fillPoint);
-                context.lineTo(width * .2, height * .95)
-                context.lineTo(width * .8, height * .95)
-                context.lineTo(right.x(fillPoint), fillPoint);
-                context.fillStyle = properties.Color;
-                context.fill();
-            }
-        },
-        properties: {
-            Fill: 0,
-            Color: "#03a9f4",
-        }
-    }
-}
+				context.beginPath();
+				context.moveTo(width * 0.1, height * 0.05);
+				context.lineTo(width * 0.2, height * 0.95);
+				context.lineTo(width * 0.8, height * 0.95);
+				context.lineTo(width * 0.9, height * 0.05);
+				context.stroke();
+
+				const left = LineCalculator(width * 0.1, height * 0.05, width * 0.2, height * 0.95);
+				const right = LineCalculator(width * 0.8, height * 0.95, width * 0.9, height * 0.05);
+
+				const cupHeight = height * 0.95 - height * 0.05;
+				const fillPoint = height * 0.05 + cupHeight * (1 - properties.Fill);
+
+				context.beginPath();
+				context.moveTo(left.x(fillPoint), fillPoint);
+				context.lineTo(width * 0.2, height * 0.95);
+				context.lineTo(width * 0.8, height * 0.95);
+				context.lineTo(right.x(fillPoint), fillPoint);
+				context.fillStyle = properties.Color;
+				context.fill();
+			}
+		},
+		properties: {
+			Fill: 0,
+			Color: '#03a9f4'
+		}
+	},
+	IceCube: {
+		draw: (canvas, width, height) => {
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
+				var imageObj1 = new Image();
+				imageObj1.onload = function() {
+					ctx.drawImage(imageObj1, 0, 0, 50, 65);
+				};
+				imageObj1.src = 'https://www.pngkey.com/png/full/558-5588414_cute-ice-cube-cartoon.png';
+				console.log(imageObj1.src);
+			}
+		},
+		properties: {}
+	},
+	Banana: {
+		draw: (canvas, width, height) => {
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
+				var imageObj1 = new Image();
+				imageObj1.onload = function() {
+					ctx.drawImage(imageObj1, 0, 0, 80, 80);
+				};
+				imageObj1.src = 'https://webstockreview.net/images/bananas-clipart-cute-4.png';
+				console.log(imageObj1.src);
+			}
+		},
+		properties: {}
+	},
+	Milk: {
+		draw: (canvas, width, height) => {
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
+				var imageObj1 = new Image();
+				imageObj1.onload = function() {
+					ctx.drawImage(imageObj1, 0, 0, 90, 90);
+				};
+				imageObj1.src = 'https://easydrawingguides.com/wp-content/uploads/2018/10/Milk-Cartoon-10.png';
+				console.log(imageObj1.src);
+			}
+		},
+		properties: {}
+	},
+	Mango: {
+		draw: (canvas, width, height) => {
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
+				var imageObj1 = new Image();
+				imageObj1.onload = function() {
+					ctx.drawImage(imageObj1, 0, 0, 70, 70);
+				};
+				imageObj1.src = 'https://cdn140.picsart.com/273156853001211.png?r1024x1024';
+				console.log(imageObj1.src);
+			}
+		},
+		properties: {}
+	},
+	Blueberry: {
+		draw: (canvas, width, height) => {
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
+				var imageObj1 = new Image();
+				imageObj1.onload = function() {
+					ctx.drawImage(imageObj1, 0, 0, 70, 70);
+				};
+				imageObj1.src =
+					'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/46d4d6d5-b829-4f7a-812f-6e1d80822126/d30hw1n-41586600-fc0e-4d78-8b77-16f1788c2b3c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ2ZDRkNmQ1LWI4MjktNGY3YS04MTJmLTZlMWQ4MDgyMjEyNlwvZDMwaHcxbi00MTU4NjYwMC1mYzBlLTRkNzgtOGI3Ny0xNmYxNzg4YzJiM2MucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.K91-BgqVgawLPHg8QCHnC2RLosmYymVgBGdyRjvQDI8';
+				console.log(imageObj1.src);
+			}
+		},
+		properties: {}
+	},
+	Strawberry: {
+		draw: (canvas, width, height) => {
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
+				var imageObj1 = new Image();
+				imageObj1.onload = function() {
+					ctx.drawImage(imageObj1, 0, 0, 80, 70);
+				};
+				imageObj1.src = 'https://image.flaticon.com/icons/png/512/284/284757.png';
+				console.log(imageObj1.src);
+			}
+		},
+		properties: {}
+	},
+	Machine: {
+		draw: (canvas, width, height) => {
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
+				var imageObj1 = new Image();
+				imageObj1.onload = function() {
+					ctx.drawImage(imageObj1, 0, 0, 75, 75);
+				};
+				imageObj1.src = 'https://espressoplanet.r.worldssl.net/images/P/Barista-Front.png';
+				console.log(imageObj1.src);
+			}
+		},
+		properties: {}
+	}
+};
 
 function createImage(toolType) {
-    const image = {};
-    image.draw = IMAGES[toolType].draw;
-    image.properties = {};
-    Object.keys(IMAGES[toolType].properties).map((key) => {
-        image.properties[key] = IMAGES[toolType].properties[key];
-    });
-    return image;
+	const image = {};
+	image.draw = IMAGES[toolType].draw;
+	image.properties = {};
+	Object.keys(IMAGES[toolType].properties).map((key) => {
+		image.properties[key] = IMAGES[toolType].properties[key];
+	});
+
+	return image;
 }
 
-export {CATEGORIES, IMAGES, createImage};
+export { CATEGORIES, IMAGES, createImage };
