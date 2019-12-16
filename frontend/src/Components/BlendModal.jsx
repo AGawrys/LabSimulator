d0db8ebc830878fec8170ee72ba163208710bc91
James Angeles <JamesAngeles21@gmail.com>
2019-12-16 11:15:59 -0500

    fixed blend modal bugs resulting from merge

--- frontend/src/Components/BlendModal.jsx
+++ frontend/src/Components/BlendModal.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import Timer from 'react-compound-timer';
+import React from "react"
+import {Modal, Button} from 'react-bootstrap';
+import Timer from "react-compound-timer";
 import Tool from './Tool.jsx';
 
 class BlendModal extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			started: false,
-			failed: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            started: false,
+            failed: false,
             completed: false,
             animation: null,
             time: null,
@@ -27,14 +27,14 @@ class BlendModal extends React.Component {
             <Timer
                 startImmediately={false}
                 onStop={this.onStop}
-				checkpoints={[{
-					time: ((this.props.time - 1) * 1000),
-					callback: () => this.setState({time: this.props.time - 1})
-				}, {
+                checkpoints={[{
+                    time: ((this.props.time - 1) * 1000),
+                    callback: () => this.setState({time: this.props.time - 1})
+                }, {
                     time: (this.props.time + 2)  * 1000,
                     callback: () => this.setState({time: this.props.time + 2})
                 }]}
-			>
+            >
                 {({ start, resume, pause, stop, reset, getTimerState, getTime, setTime }) =>  {
                     if (started) {
                         start();
@@ -101,10 +101,14 @@ class BlendModal extends React.Component {
         );
     }
 
-	onHide = () => {
-		this.resetState();
-		this.props.onComplete();
-	};
+    resetState() {
+        this.setState({
+            started: false,
+            completed: false,
+            failed: false,
+            time: null
+        });
+    }
 
     onStop() {
         if (this.state.time >= this.props.time - 1 && this.state.time <= this.props.time + 1) {
@@ -130,58 +134,59 @@ class BlendModal extends React.Component {
         this.props.onComplete();
     }
 
-		target.image.animation.shake = true;
-		target.image.animation.reset = !reset;
+    onBlend() {
+        const {target} = this.props
+        const {ramp, rock, increasing, reset}  = target.image.animation
 
-		const animation = window.requestAnimationFrame(this.onBlend);
-		this.setState({ animation });
+        target.image.animation.shake = true;
+        target.image.animation.reset = !reset;
 
-		if (ramp < 1) {
-			target.image.animation.ramp += 0.1;
-		} else {
-			if (increasing) {
-				target.image.animation.rock += 0.1;
-				target.image.animation.increasing = !(target.image.animation.rock >= 1);
-			} else {
-				target.image.animation.rock -= 0.1;
-				target.image.animation.increasing = target.image.animation.rock <= 0;
-			}
-		}
-	}
+        const animation = window.requestAnimationFrame(this.onBlend);
+        this.setState({animation});
 
-	onBlendEnd() {
-		const { target } = this.props;
-		const { ramp, rock, increasing, shake } = target.image.animation;
+        if (ramp < 1) {
+            target.image.animation.ramp += .10
+        } else {
+            if (increasing) {
+                target.image.animation.rock += .1;
+                target.image.animation.increasing = !(target.image.animation.rock >= 1) 
+            }
+            else {
+                target.image.animation.rock -= .1;
+                target.image.animation.increasing = (target.image.animation.rock <= 0)
+            }
+        }
+    }
 
-		target.image.animation.shake = false;
-		target.image.animation.resetX = 0;
-		target.image.animation.resetY = 0;
+    onBlendEnd() {
+        const {target} = this.props
+        const {ramp, rock, increasing, shake}  = target.image.animation
 
-		if (target.image.animation.ramp === 0 && target.image.animation.rock === 0.5) {
-			window.cancelAnimationFrame(this.state.animation);
-		} else {
-			const animation = window.requestAnimationFrame(this.onBlendEnd);
-			this.setState({ animation });
-		}
+        target.image.animation.shake = false;
+        target.image.animation.resetX = 0;
+        target.image.animation.resetY = 0;
 
-		if (target.image.animation.ramp > 0) {
-			target.image.animation.ramp -= 0.2;
-			if (target.image.animation.ramp < 0.1 && target.image.animation.ramp > -0.1) {
-				target.image.animation.ramp = 0;
-			}
-		} else {
-			target.image.animation.ramp = 0;
-		}
-		if (target.image.animation.rock != 0.5) {
-			if (rock > 0.6) {
-				target.image.animation.rock -= 0.1;
-			} else if (rock < 0.4) {
-				target.image.animation.rock += 0.1;
-			} else {
-				target.image.animation.rock = 0.5;
-			}
-		}
-	}
+        if (target.image.animation.ramp === 0 && target.image.animation.rock === .5) {
+            window.cancelAnimationFrame(this.state.animation);
+        } else {
+            const animation = window.requestAnimationFrame(this.onBlendEnd);
+            this.setState({animation});
+        }
+        
+        if (target.image.animation.ramp > 0) {
+            target.image.animation.ramp -= .2;
+            if (target.image.animation.ramp < .1 && target.image.animation.ramp > -.1) {
+                target.image.animation.ramp = 0;
+            }
+        } else {
+            target.image.animation.ramp = 0;
+        }
+        if (target.image.animation.rock != .5) {
+            if (rock > .6) {target.image.animation.rock -= .1}
+            else  if (rock < .4) {target.image.animation.rock += .1}
+            else {target.image.animation.rock = .5}
+        }
+    }
 }
 
 export default BlendModal;
