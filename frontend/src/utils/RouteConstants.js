4a033bd31dad0f8b2859ea698fef14176221cf9b
Steven Kuang <stevenkuang951@gmail.com>
2019-12-16 23:35:31 -0500

    aws setup

--- frontend/src/utils/RouteConstants.js
+++ frontend/src/utils/RouteConstants.js
@@ -6,18 +6,18 @@ const ORGANIZATION = 'organization';
 
 const Routes = {
 	DEFAULT: '/',
-	INSTRUCTOR_EDITOR: '/' + INSTRUCTOR + '/' + EDITOR + "/",
-	STUDENT_EDITOR: '/' + STUDENT + '/' + EDITOR + "/",
+	INSTRUCTOR_EDITOR: '/' + INSTRUCTOR + '/' + EDITOR + '/',
+	STUDENT_EDITOR: '/' + STUDENT + '/' + EDITOR + '/',
 	INSTRUCTOR_DASHBOARD: '/' + INSTRUCTOR + '/' + DASHBOARD,
-	INSTRUCTOR_PREVIEW: '/' + INSTRUCTOR + '/' + "preview/",
+	INSTRUCTOR_PREVIEW: '/' + INSTRUCTOR + '/' + 'preview/',
 	STUDENT_DASHBOARD: '/' + STUDENT + '/' + DASHBOARD,
 	ACCOUNT: '/account',
 	ORGANIZATION_DASHBOARD: '/' + ORGANIZATION + '/' + DASHBOARD,
 	COURSE: '/course/',
 	SIGN_UP: '/signup',
 	SEARCH: '/search/',
-	SERVER: 'http://localhost:8080/',
-	NOT_FOUND: '/404',
+	SERVER: 'http://ec2-18-220-246-194.us-east-2.compute.amazonaws.com:8080/',
+	NOT_FOUND: '/404'
 };
 
 export default Routes;
